63eb9c0f98dd1ab8a5e6df52043a4ac80e81845b
francis mwaniki <francis@kali.franc>
2024-01-08 22:14:01 +0300

    Better Random Verses

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Youtube downloader',
-  description: 'Download',
+  title: 'Random | verse',
+  description: 'Verses',
 }
 
 export default function RootLayout({
